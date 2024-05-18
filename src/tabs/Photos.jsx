064f5099f3ad23b2fc7a3de47c3e5ae3b17b9078
Kateryna Kocheharova <kot41901@gmail.com>
2024-05-18 15:34:30 +0300

    2

--- src/tabs/Photos.jsx
+++ src/tabs/Photos.jsx
@@ -1,9 +1,46 @@
-// import { getPhotos } from 'apiService/photos';
-import { Text } from 'components';
+import { getPhotos } from 'apiService/photos';
+import { Form, Text } from 'components';
+import { useState, useEffect } from 'react';
 
 export const Photos = () => {
+  const [query, setQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [images, setImages] = useState([]);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isEmpty, setIsEmpty] = useState(false);
+
+  useEffect(() => {
+    if (!query) return;
+    const getImages = async () => {
+      setIsLoading(true);
+      try {
+        const { photos, per_page, total_results } = await getPhotos(
+          query,
+          page,
+        );
+        if (!photos.length) {
+          setIsEmpty(true);
+          return;
+        }
+        setImages(prevImages => [...prevImages, ...photos]);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    getImages();
+  }, [query, page]);
+
+  const handleSubmit = query => {
+    setQuery(query);
+  };
+
   return (
     <>
+      <Form onSubmit={handleSubmit} />
       <Text textAlign="center">Let`s begin search 🔎</Text>
     </>
   );
